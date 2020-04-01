41601d1d6c5cbbd7bec097c2891cca29571b8b43
坂上計介 <iwayasunset@gmail.com>
2020-04-01 22:27:17 +0900

    update
    リンクの修正

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -4,71 +4,71 @@
   var infoWindow = [];
   var markerData = [ 
       {
-      name: 'エンジニアカフェ<br>	<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=エンジニアカフェ&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo1.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=エンジニアカフェ&dirflg=w" target="_blank">エンジニアカフェ</a><br><img src="/demo1.jpg"class=image > ',
       lat: 33.593152,
       lng: 130.401884
       }, {
-      name: 'カフェベローチェ<br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=カフェベローチェ&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo22.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=カフェベローチェ&dirflg=w" target="_blank">カフェベローチェ</a><br><img src="/demo22.jpg"class=image > ',
       lat: 33.592187,
       lng: 130.399474
       }, {
-      name: 'BOOK & CO. <br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=ブックアンドコー&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo30.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=ブックアンドコー&dirflg=w" target="_blank">BOOK & CO.</a><br><img src="/demo30.jpg"class=image > ',
       lat: 33.593852,
       lng: 130.39835
       }, {
-      name: 'タリーズ 天神店 <br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=タリーズ天神店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo40.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=タリーズ天神店&dirflg=w" target="_blank">タリーズ 天神店</a><br><img src="/demo40.jpg"class=image > ',
       lat: 33.591618,
       lng: 130.396871
       },{
-      name: 'タリーズ 新天町店 <br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=タリーズ新天町店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo50.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=タリーズ新天町店&dirflg=w" target="_blank">タリーズ 新天町店</a><br><img src="/demo50.jpg"class=image > ',
       lat: 33.590074,
       lng: 130.397071
       }, {
-      name: 'スタバ 天神VIORO店<br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタバ天神VIORO店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo60.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタバ天神VIORO店&dirflg=w" target="_blank">スタバ 天神VIORO店</a><br><img src="/demo60.jpg"class=image > ',
       lat: 33.589645 ,
       lng: 130.398415
       },{
-      name: 'スタバ メディアモール天神 <br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタバメディアモール天神&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo70.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタバメディアモール天神&dirflg=w" target="_blank">スタバ メディアモール天神</a><br><img src="/demo70.jpg"class=image > ',
       lat: 33.590907,
       lng: 130.400564
       },{
-      name: 'コメダ天神南店 <br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=コメダ天神南店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo80.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=コメダ天神南店&dirflg=w" target="_blank">コメダ天神南店</a><br><img src="/demo80.jpg"class=image > ',
       lat: 33.589295,
       lng: 130.403634
       },{
-      name: 'スタバ天神地下街店<br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタバ天神地下街店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo90.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタバ天神地下街店&dirflg=w" target="_blank">スタバ天神地下街店</a><br><img src="/demo90.jpg"class=image > ',
       lat: 33.588061,
       lng: 130.401138
       }, {
-      name: 'エスプレッサメンテ <br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=エスプレッサメンテ&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo100.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=エスプレッサメンテ&dirflg=w" target="_blank">エスプレッサメンテ</a><br><img src="/demo100.jpg"class=image > ',
       lat: 33.591907 ,
       lng: 130.398294
       },{
-      name: 'VITO中洲ゲイツ店 <br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=VITO中洲ゲイツ店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo141.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=VITO中洲ゲイツ店&dirflg=w" target="_blank">VITO中洲ゲイツ店</a><br><img src="/demo141.jpg"class=image > ',
       lat: 33.593775,
       lng: 130.406223
       }, {
-      name: 'MUJI 天神大名<br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=MUJI天神大名&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo110.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=MUJI天神大名&dirflg=w" target="_blank">MUJI 天神大名</a><br><img src="/demo110.jpg"class=image > ',
       lat: 33.587337,
       lng: 130.396749
       },  {
-      name: 'スタバ天神西通店<br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタバ天神西通店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo120.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタバ天神西通店&dirflg=w" target="_blank">スタバ天神西通店</a><br><img src="/demo120.jpg"class=image > ',
       lat: 33.587888,
       lng: 130.396641
       }, {
-      name: '福岡市 スタートアップカフェ<br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタートアップカフェ&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo130.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=スタートアップカフェ&dirflg=w" target="_blank">スタートアップカフェ</a><br><img src="/demo130.jpg"class=image > ',
       lat: 33.589162 ,
       lng: 130.395307
       },{
-      name: 'マクド キャナルシティ博多店<br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=マクドナルドキャナルシティ博多店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo1.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=マクドナルドキャナルシティ博多店&dirflg=w" target="_blank">マクド キャナルシティ博多店</a><br><img src="/demo1.jpg"class=image > ',
       lat: 33.58956,
       lng: 130.411763
       },{
-      name: 'サブウェイ 天神ロフト店<br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=サブウェイ天神ロフト店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo1.jpg"class=image > ',
+      name: 'サブウェイ 天神ロフト店<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=サブウェイ天神ロフト店&dirflg=w" target="_blank">サブウェイ 天神ロフト店</a><br><img src="/demo1.jpg"class=image > ',
       lat: 33.58695 ,
       lng: 130.401579
       },{
-      name: '黒猫屋珈琲店<br><a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=黒猫屋珈琲店&dirflg=w" target="_blank">ルート案内</a><br><img src="/demo1.jpg"class=image > ',
+      name: '<a href="https://maps.google.com/maps?saddr=33.588689,130.399967&daddr=黒猫屋珈琲店&dirflg=w" target="_blank">黒猫屋珈琲店</a><br><img src="/demo1.jpg"class=image > ',
       lat: 33.585335 ,
       lng: 130.391875
       }
@@ -93,7 +93,7 @@
                             // マップオプションを変数に格納
           var mymapOptions = {
             zoom : 16,                 // 拡大倍率
-            center : { lat: 33.588689,lng: 130.399967 },     // myLatLng, // 緯度・経度
+            center : { lat: 33.588689,lng: 130.399967 },     // myLatLng, // 緯度・経度　SSL化までは使わない
             maxZoom:20,                // 最大ズームの設定
             minZoom: 15,               // 最小ズームの設定
             scaleControl:true,         // 距離スケール表示
@@ -104,7 +104,7 @@
             document.getElementById("map"),   // マップを表示する要素
             mymapOptions                      // マップオプション
           );
-          
+
  
       // ========== 以下が現在位置Circleのコード ==========
       circleOptions = {
@@ -136,7 +136,8 @@
               });
 
               infoWindow[i] = new google.maps.InfoWindow({ 
-                content: "<p style='width:120px; font-size:10px; text-align:center;'>" + markerData[i]['name'] + '</div>' 
+                content: "<p style='width:120px; text-decoration: none; font-size:10px; text-align:center;'>"
+                 + markerData[i]['name'] + '</div>' 
               });
               markerEvent(i); 
           }
